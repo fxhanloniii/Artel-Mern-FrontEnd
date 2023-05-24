418aec278a790abf227b311d5401d142cca8f156
fxhanloniii <hanlonfrankiex@gmail.com>
2023-05-23 19:10:16 -0700

    added edit caption

--- src/components/EditDelete.js
+++ src/components/EditDelete.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const EditDelete = ({ handleDelete }) => {
+
+const EditDelete = ({ handleDelete, setEditing }) => {
   return (
     <>
-    <button onClick={handleDelete} className='delete submit bg-gradient-to-r from-blue-500 to-purple-500'>Delete</button>
-    <button className='edit submit bg-gradient-to-r from-blue-500 to-purple-500'>Edit</button>
+    <button onClick={handleDelete} className='delete bg-gradient-to-r from-blue-500 to-purple-500'>Delete</button>
+    <button className='edit bg-gradient-to-r from-blue-500 to-purple-500' onClick={() => setEditing(true)}>Edit Caption</button>
     </>
   )
 }
--- src/pages/ShowPost.js
+++ src/pages/ShowPost.js
@@ -6,9 +6,11 @@ import EditDelete from '../components/EditDelete';
 import { getUserToken } from '../utils/authToken';
 
 const ShowPost = ({ comment, user }) => {
-    const [post, setPost] = useState({})
+    const [post, setPost] = useState({});
     const [loading, setLoading] = useState(true);
     const [comments, setComments] = useState([]);
+    const [caption, setCaption] = useState('');
+    const [editing, setEditing] = useState(false);
     const { id } = useParams();
     const navigate = useNavigate();
     
@@ -18,8 +20,9 @@ const ShowPost = ({ comment, user }) => {
             const response = await fetch(`http://localhost:4000/art/${id}`);
             const data = await response.json();
             console.log(data)
-            console.log(user.username)
             setPost(data)
+            setCaption(data.post.caption)
+            console.log(caption)
             setComments(data.comments)
             setLoading(false);
         } catch (err) {
@@ -46,6 +49,28 @@ const ShowPost = ({ comment, user }) => {
             console.error('Error deleting post', err)
         }
     }
+
+    const handleEdit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true)
+            const response = await fetch(`http://localhost:4000/art/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${getUserToken()}`,
+                },
+                body: JSON.stringify({ caption }),
+            });
+            const data = await response.json();
+            setPost(data);
+            setCaption(data.caption)
+            setEditing(false);
+            fetchPost();
+        } catch (err) {
+            console.error('Error deleting post', err)
+        }
+    }
     if (loading) {
         return <h1>Loading...</h1>
     }
@@ -54,7 +79,17 @@ const ShowPost = ({ comment, user }) => {
   return (
     <div className='showPage'>
         <div>
+            {editing ? ( 
+                <form className='editForm' onSubmit={handleEdit}>
+                <input type='text' placeholder={post.post.caption} value={caption} onChange={(e) => setCaption(e.target.value)} className='caption'/>
+                <div className='showPostBtns'>
+                <button type='submit' value='Save' className='save bg-gradient-to-r from-blue-500 to-purple-500'>Save</button>
+                <button onClick={() => setEditing(false)} className='save bg-gradient-to-r from-blue-500 to-purple-500'>Cancel</button>
+                </div>
+                </form>
+            ) : ( 
             <p className='caption'>{post.post.caption}</p>
+            )}
         </div>
         <div className='heroPost'>
             <div className='slideshow'>
@@ -69,7 +104,7 @@ const ShowPost = ({ comment, user }) => {
                 </div>
         </div>
         <div className='showPostBtns'>   
-           {user._id === post.post.user && <EditDelete handleDelete={handleDelete}/> }
+           {user._id === post.post.user && <EditDelete handleDelete={handleDelete} setEditing={setEditing}/> }
         </div>
         <div>
             <CommentForm postId={id} comment={comment} comments={comments} setComments={setComments} fetchPost={fetchPost}/>
