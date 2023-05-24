d0ec606dcc3ea13f269dea61425e0baaa6a11a4d
fxhanloniii <hanlonfrankiex@gmail.com>
2023-05-23 17:21:09 -0700

    added delete button for user posts

--- src/components/EditDelete.js
+++ src/components/EditDelete.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const EditDelete = () => {
+const EditDelete = ({ handleDelete }) => {
   return (
-    <div>EditDelete</div>
+    <>
+    <button onClick={handleDelete} className='delete submit bg-gradient-to-r from-blue-500 to-purple-500'>Delete</button>
+    <button className='edit submit bg-gradient-to-r from-blue-500 to-purple-500'>Edit</button>
+    </>
   )
 }
 
--- src/pages/ShowPost.js
+++ src/pages/ShowPost.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import CommentForm from '../components/CommentForm';
 import EditDelete from '../components/EditDelete';
+import { getUserToken } from '../utils/authToken';
 
 const ShowPost = ({ comment, user }) => {
     const [post, setPost] = useState({})
     const [loading, setLoading] = useState(true);
-    const [comments, setComments] = useState([])
+    const [comments, setComments] = useState([]);
     const { id } = useParams();
+    const navigate = useNavigate();
     
 
     const fetchPost = async () => {
@@ -16,6 +18,7 @@ const ShowPost = ({ comment, user }) => {
             const response = await fetch(`http://localhost:4000/art/${id}`);
             const data = await response.json();
             console.log(data)
+            console.log(user.username)
             setPost(data)
             setComments(data.comments)
             setLoading(false);
@@ -29,12 +32,30 @@ const ShowPost = ({ comment, user }) => {
         fetchPost();
     }, [])
 
+    const handleDelete = async () => {
+        try {
+            const response = await fetch(`http://localhost:4000/art/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${getUserToken()}`,
+                },
+            })
+            navigate(`/user/profile/${user.username}`)
+        } catch (err) {
+            console.error('Error deleting post', err)
+        }
+    }
     if (loading) {
         return <h1>Loading...</h1>
     }
+   
 
   return (
     <div className='showPage'>
+        <div>
+            <p className='caption'>{post.post.caption}</p>
+        </div>
         <div className='heroPost'>
             <div className='slideshow'>
                 <img src={post.post.image } alt='user post' className='slideshowImg' />
@@ -48,7 +69,7 @@ const ShowPost = ({ comment, user }) => {
                 </div>
         </div>
         <div className='showPostBtns'>   
-           {user._id === post.post.user && <EditDelete /> }
+           {user._id === post.post.user && <EditDelete handleDelete={handleDelete}/> }
         </div>
         <div>
             <CommentForm postId={id} comment={comment} comments={comments} setComments={setComments} fetchPost={fetchPost}/>
